ff25e3b87d931a104ce24f30dedc6f00eb270162
Jayesh Dhandha <jayudhandha@gmail.com>
2021-04-18 11:31:17 +0530

    Add files via upload

--- controllers/person.js
+++ controllers/person.js
@@ -7,28 +7,14 @@ exports.listPersons = (req, res) => {
   const limit = +req.query.limit
   const start = +req.query.start
 
-  if(limit && start) {
-    
-    for(i = start; i < start + limit; i++){
-      results.push(persons[i]);
-    }
-
-    res.status(200).json({
-      message: "Person fetched successfully!",
-      persons: results,
-      total: results.length
-    });
-  } else {
-    // Return whole data
-    res.status(200).json({
-      message: "Person fetched successfully!",
-      persons: persons,
-      total: persons.length
-    });
+  for(i = start; i < start + limit; i++){
+    results.push(persons[i]);
   }
 
-
+  res.status(200).json({
+    message: "Person fetched successfully!",
+    persons: results,
+    total: results.length
+  });
 
 }
-
-
