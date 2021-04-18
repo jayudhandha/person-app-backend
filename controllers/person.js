2935a443beda56576dce428e675ade115deddd92
Jayesh Dhandha <jayudhandha@gmail.com>
2021-04-18 11:50:27 +0530

    added logs

--- controllers/person.js
+++ controllers/person.js
@@ -3,9 +3,15 @@ const persons = require('../resources/Persons.json')
 const results = [];
 
 exports.listPersons = (req, res) => {
-  console.log(req.query)
+  
+  console.log('Before Start: '+start)
+  console.log('Before limit: '+limit)
   const limit = req.query.limit
   const start = req.query.start
+
+  
+  console.log('After Start: '+start)
+  console.log('After limit: '+limit)
 
   for(i = start; i < start + limit; i++){
     results.push(persons[i]);
