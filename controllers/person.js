274220f60f1496d168ebdc06622f51354330165d
Jayesh Dhandha <jayudhandha@gmail.com>
2021-04-19 21:30:59 +0530

    Update person.js

--- controllers/person.js
+++ controllers/person.js
@@ -3,18 +3,43 @@ const persons = require('../resources/Persons.json')
 exports.listPersons = (req, res) => {
 
   const results = [];
-
+  let filteredPersons = persons
   const limit = parseInt(req.query.limit)
   const start = parseInt(req.query.start)
+  const name = req.query.name
+  
+  console.log("NAME: "+name);
+  if (name !== undefined) {
+    filteredPersons = filterByeName(name);
+  } 
 
-  for(i = start; i < start + limit; i++){
-    results.push(persons[i]);
+  if (start < 0 || limit < 0) {
+    return res.status(400).json({
+      message: "start and limit should be positive number!"
+    });
+  }
+
+  const totalRows = start + limit;
+  const lastIndex = totalRows > filteredPersons.length ? filteredPersons.length : totalRows 
+  for(i = start; i < lastIndex ; i++) {
+    results.push(filteredPersons[i]);
   }
 
   res.status(200).json({
     message: "Persons fetched successfully!",
     persons: results,
-    total: persons.length
+    total: filteredPersons.length
   });
 
+}
+
+function filterByeName(name) {
+  let filteredPersons = []
+  for(i = 0; i < persons.length ; i++) {
+    if(persons[i]['name'] === name) {
+      filteredPersons.push(persons[i])
+    }
+  }
+
+  return filteredPersons;
 }
